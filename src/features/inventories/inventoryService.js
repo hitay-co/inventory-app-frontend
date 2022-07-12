43b377a23d1415e32c31538422b4962e0e41c7e9
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-07-12 18:45:18 +0300

    default  files coming with cra deleted. since there is only 1 api (no extra dev api or test api), env file deleted.

--- src/features/inventories/inventoryService.js
+++ src/features/inventories/inventoryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = 'https://n11-inventory-api.herokuapp.com';
 const API_URL = BASE_URL + '/api/inventories';
 
 const getInventories = async () => {
